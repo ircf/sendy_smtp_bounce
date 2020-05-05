911ceddbac7cf0118e121b527b99f57610bd2c70
jamessampford <jamessampford@supanet.com>
2020-05-05 09:37:18 +0100

    Fixes subject response

--- includes/helpers/bounce_driver.class.php
+++ includes/helpers/bounce_driver.class.php
@@ -515,7 +515,7 @@ class BounceHandler{
         }
         // special formatting
         $hash['Received']= @explode('|', $hash['Received']);
-        $hash['Subject'] = isset($hash['Subject']) ? : '';
+        $hash['Subject'] = isset($hash['Subject']) ? $hash['Subject'] : '';
         return $hash;
     }
 
